0540c68ff33af21535999d595b5bc3d5e4b50fd7
Maruf21 <marufshah2500@gmail.com>
2020-09-13 23:22:36 +0600

    Changes: - Added dynamic localization TODO: - Updating changes for every language

---
+++ Blitz Troubleshooter V2.3/LocUtil.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Windows;
+
+namespace Blitz_Troubleshooter_V2._3
+{
+    public static class LocUtil
+    {
+        /// <summary>
+        /// Get application name from an element
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        private static string getAppName(FrameworkElement element)
+        {
+            var elType = element.GetType().ToString();
+            var elNames = elType.Split('.');
+
+            return elNames[0];
+        }
+
+        /// <summary>
+        /// Generate a name from an element base on its class name
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        private static string getElementName(FrameworkElement element)
+        {
+            var elType = element.GetType().ToString();
+            var elNames = elType.Split('.');
+
+            var elName = "";
+            if (elNames.Length >= 2)
+                elName = elNames[elNames.Length - 1];
+
+            return elName;
+        }
+
+        /// <summary>
+        /// Get current culture info name base on previously saved setting if any,
+        /// otherwise get from OS language
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static string GetCurrentCultureName(FrameworkElement element)
+        {
+            RegistryKey curLocInfo = Registry.CurrentUser.OpenSubKey("GsmLib" + @"\" + getAppName(element), false);
+
+            var cultureName = CultureInfo.CurrentUICulture.Name; //Console.WriteLine(cultureName);
+            if (curLocInfo != null)
+            {
+                cultureName = curLocInfo.GetValue(getElementName(element) + ".localization", "en-US").ToString();
+            }
+
+            return cultureName;
+        }
+
+        /// <summary>
+        /// Set language based on previously save language setting,
+        /// otherwise set to OS lanaguage
+        /// </summary>
+        /// <param name="element"></param>
+        public static void SetDefaultLanguage(FrameworkElement element)
+        {
+            SetLanguageResourceDictionary(element, GetLocXAMLFilePath(getElementName(element), GetCurrentCultureName(element)));
+        }
+
+        /// <summary>
+        /// Dynamically load a Localization ResourceDictionary from a file
+        /// </summary>
+        public static void SwitchLanguage(FrameworkElement element, string inFiveCharLang)
+        {
+            /////// Get previously saved localization info
+            //var elType = element.GetType().ToString();
+            //var elNames = elType.Split('.');
+            //var appName = elNames[0];
+            //var elName = elNames[elNames.Length - 1];
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(inFiveCharLang);
+
+            SetLanguageResourceDictionary(element, GetLocXAMLFilePath(getElementName(element), inFiveCharLang));
+
+            // Save new culture info to registry
+            RegistryKey UserPrefs = Registry.CurrentUser.OpenSubKey("GsmLib" + @"\" + getAppName(element), true);
+
+            if (UserPrefs == null)
+            {
+                // Value does not already exist so create it
+                RegistryKey newKey = Registry.CurrentUser.CreateSubKey("GsmLib");
+                UserPrefs = newKey.CreateSubKey(getAppName(element));
+            }
+
+            UserPrefs.SetValue(getElementName(element) + ".localization", inFiveCharLang);
+        }
+
+        /// <summary>
+        /// Returns the path to the ResourceDictionary file based on the language character string.
+        /// </summary>
+        /// <param name="inFiveCharLang"></param>
+        /// <returns></returns>
+        public static string GetLocXAMLFilePath(string element, string inFiveCharLang)
+        {
+            string locXamlFile = element + "." + inFiveCharLang + ".xaml";
+            string directory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(directory, "i18N", locXamlFile);
+        }
+
+        /// <summary>
+        /// Sets or replaces the ResourceDictionary by dynamically loading
+        /// a Localization ResourceDictionary from the file path passed in.
+        /// </summary>
+        /// <param name="inFile"></param>
+        private static void SetLanguageResourceDictionary(FrameworkElement element, String inFile)
+        {
+            if (File.Exists(inFile))
+            {
+                // Read in ResourceDictionary File
+                var languageDictionary = new ResourceDictionary();
+                languageDictionary.Source = new Uri(inFile);
+
+                // Remove any previous Localization dictionaries loaded
+                int langDictId = -1;
+                for (int i = 0; i < element.Resources.MergedDictionaries.Count; i++)
+                {
+                    var md = element.Resources.MergedDictionaries[i];
+                    //Console.WriteLine("MergedDictionaries[i] : " + md["ResourceDictionaryName"].ToString());
+
+                    // Make sure your Localization ResourceDictionarys have the ResourceDictionaryName
+                    // key and that it is set to a value starting with "Loc-".
+                    if (md.Contains("ResourceDictionaryName"))
+                    {
+                        if (md["ResourceDictionaryName"].ToString().StartsWith("Loc-"))
+                        {
+                            langDictId = i;
+                            break;
+                        }
+                    }
+                }
+                if (langDictId == -1)
+                {
+                    // Add in newly loaded Resource Dictionary
+                    element.Resources.MergedDictionaries.Add(languageDictionary);
+                }
+                else
+                {
+                    // Replace the current langage dictionary with the new one
+                    element.Resources.MergedDictionaries[langDictId] = languageDictionary;
+                }
+            }
+            else
+            {
+                MessageBox.Show("'" + inFile + "' not found.");
+            }
+        }
+    }
+
+}
--- Blitz Troubleshooter V2.3/MainWindow.xaml.cs
+++ Blitz Troubleshooter V2.3/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Blitz_Troubleshooter_V2._3
 {
@@ -10,38 +18,182 @@ namespace Blitz_Troubleshooter_V2._3
         public MainWindow()
         {
             InitializeComponent();
+            LocUtil.SetDefaultLanguage(this);
+
+            // Adjust checked language menu item
+            foreach (MenuItem item in menuItemLanguages.Items)
+            {
+                if (item.Tag.ToString().Equals(LocUtil.GetCurrentCultureName(this)))
+                    item.IsChecked = true;
+            }
         }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (MenuItem item in menuItemLanguages.Items)
+            {
+                item.IsChecked = false;
+            }
 
+            MenuItem mi = sender as MenuItem; //Console.WriteLine("menu tag: " + mi.Tag.ToString());
+            mi.IsChecked = true;
+            //SwitchLanguage(mi.Tag.ToString());
+            LocUtil.SwitchLanguage(this, mi.Tag.ToString());
+        }
+        public static string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public static string localappdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public string[] paths = new String[] {
+        localappdata + "\\blitz-updater",
+        localappdata + "\\Programs\\blitz-core",
+        appdata + "\\Blitz",
+        appdata + "\\blitz-core",
+        appdata + "\\Blitz-helpers",
+        localappdata + "\\Programs\\Blitz"
+        };
+        public string path = Path.GetTempPath();
+        //Is league client running
+        public static bool IsRunning(string processName)
+        {
+            try
+            {
+                var retVal = Process.GetProcesses().Any(p => p.ProcessName.Contains(processName));
+                return retVal;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        //Get league clinet path
+        public static string getClientPath(string processName)
+        {
+            List<string> mainPaths = new List<string>();
+            Process[] leagueClient = Process.GetProcessesByName(processName);
+            string leaguePath = leagueClient[0].MainModule.FileName;
+            string[] pathStrings = leaguePath.Split('\\');
 
+            for (var i = 0; i < pathStrings.Length - 1; i++)
+            {
+                mainPaths.Add(pathStrings[i]);
+            }
+            var mainPath = string.Join<string>("\\", mainPaths);
+            return mainPath;
+        }
+        public void KillBlitz()
+        {
+            foreach (Process proc in Process.GetProcessesByName("Blitz"))
+            {
+                proc.Kill();
+            }
+
+        }
+        public void AppdataBlitz()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Blitz";
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+        }
+        public void Uninstall()
+        {
+            foreach (var path in paths)
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
+        Stopwatch sw = new Stopwatch();
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            labelspeed.Text = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+        public void enableBtn()
+        {
+            btnStartDownload.IsEnabled = true;
+            btn3.IsEnabled = true;
+            btn4.IsEnabled = true;
+            btn1.IsEnabled = true;
+        }
+        public void disableBtn()
+        {
+            btnStartDownload.IsEnabled = false;
+            btn1.IsEnabled = false;
+            btn3.IsEnabled = false;
+            btn4.IsEnabled = false;
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download Completed, Pleaase press OK and continue on installation window.", "Download Completed", MessageBoxButton.OK, MessageBoxImage.Information);
+            input_text.Text = "Download Completed";
+            labelspeed.Text = null;
+            enableBtn();
+            Process.Start(path + "temp.exe");
+            sw.Reset();
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window English = new English();
-            English.Show();
-            Close();
 
+            KillBlitz();
+            System.Threading.Thread.Sleep(2000);
+            Uninstall();
+            WebClient client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+            // Starts the download
+            sw.Start();
+            client.DownloadFileAsync(new Uri("https://blitz.gg/download/win"), path + "temp.exe");
+            input_text.Text = "Downloading Blitz.exe";
+            disableBtn();
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Window German = new German();
-            German.Show();
-            Close();
+            WebClient client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+            // Starts the download
+            sw.Start();
+            client.DownloadFileAsync(new Uri("https://aka.ms/vs/16/release/vc_redist.x86.exe"), path + "temp.exe");
+            input_text.Text = "Downloading vc_redist.x86.exe";
+            disableBtn();
         }
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            Window Turkish = new Turkish();
-            Turkish.Show();
-            Close();
+            KillBlitz();
+            System.Threading.Thread.Sleep(1000);
+            AppdataBlitz();
+            MessageBox.Show("Cache successfully cleared");
         }
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Welcome to my Unofficial Troubleshooter for the Blitz application, if you've pressed this button it means you've probably been in the need of some information regarding the program." + "\n\n" + "There are currently 4 options to choose from in order to solve your issues" + "\n\n" + "If you'd like some detailed information regarding these options, please message me on Discord:" + "\n" + "Kubi#2468", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            KillBlitz();
+            System.Threading.Thread.Sleep(1000);
+            Uninstall();
+            MessageBox.Show("Blitz has been uninstalled", "Blitz Uninstallation", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-
-        private void btn4_Click(object sender, RoutedEventArgs e)
+        //Removes Champions folder
+        private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            Window Polish = new Polish();
-            Polish.Show();
-            Close();
+            if (IsRunning("LeagueClient"))
+            {
+                var path = getClientPath("LeagueClient") + @"\Config\Champions";
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+                MessageBox.Show("All Build has been cleared", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("League Client is not running. You need to have league client running in order to clear builds.", "League Client", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
