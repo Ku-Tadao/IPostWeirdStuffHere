4493d157d291bf13517f565184d45870f0f54789
Kubi12344 <kubilay12344@hotmail.nl>
2020-11-01 05:11:55 +0100

    Update GoogleSearch.py

--- Discord_Python_Bot/cogs/GoogleSearch.py
+++ Discord_Python_Bot/cogs/GoogleSearch.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import requests
+import grequests
 import urllib
 
 class League_of_Legends(commands.Cog, name="League of Legends"):
@@ -9,8 +9,7 @@ class League_of_Legends(commands.Cog, name="League of Legends"):
 
     def __init__(self, client):
         self.client = client
-   
-    
+
     @commands.command()
     async def google(self, ctx, *, givenquery):
         headers = {
@@ -21,7 +20,7 @@ class League_of_Legends(commands.Cog, name="League of Legends"):
             "q": givenquery,
             "num": 10
         }
-        resp = requests.get("https://rapidapi.p.rapidapi.com/api/v1/search/" + urllib.parse.urlencode(query), headers=headers)
+        resp = grequests.get("https://rapidapi.p.rapidapi.com/api/v1/search/" + urllib.parse.urlencode(query), headers=headers)
         results = resp.json()
         resultaten = results["results"]
         embed=discord.Embed(title="Search results for:", description=givenquery)
@@ -29,24 +28,6 @@ class League_of_Legends(commands.Cog, name="League of Legends"):
         for resultaat in resultaten:
             embed.add_field(name=resultaat["title"], value=resultaat["link"], inline=False)
         await ctx.channel.send(embed=embed)
-
-    @commands.command()
-    async def youtube(self, ctx, *, givenquery):
-
-        url = "https://youtube-search1.p.rapidapi.com/the%2520beatles"
-
-        headers = {
-            'x-rapidapi-host': "youtube-search1.p.rapidapi.com",
-            'x-rapidapi-key': "0519994bedmsh6d54e5250fddec6p1bfba4jsn8d8846906f0e" 
-            }
-
-        response = requests.request("GET", url, headers=headers)
-
-        print(response.text)
-
-
-         
-
-    
+        
 def setup(client):
     client.add_cog(League_of_Legends(client))
