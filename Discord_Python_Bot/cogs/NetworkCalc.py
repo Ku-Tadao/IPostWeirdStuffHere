4ab38d88c0086948ad54d8705f156f98b3388ee6
Kubi12344 <kubilay12344@hotmail.nl>
2020-12-17 13:30:47 +0100

    changes, updates, messing the code up even more

--- Discord_Python_Bot/cogs/NetworkCalc.py
+++ Discord_Python_Bot/cogs/NetworkCalc.py
@@ -4,8 +4,8 @@ import requests
 import bs4 
 from bs4 import BeautifulSoup as bs
 
-class NetworkCalc(commands.Cog, name="IP / Subnet Calculator"):
-    """IP / Subnet mask calculator"""
+class NetworkCalc(commands.Cog, name="\nIP / Subnet Calculator"):
+    """\nIP / Subnet mask calculator"""
 
 
     def __init__(self, client):
@@ -15,7 +15,7 @@ class NetworkCalc(commands.Cog, name="IP / Subnet Calculator"):
     @commands.command()
     async def ncalc(self, ctx, adress, netmask):
         netmask = int(netmask)
-        if netmask <= 32:
+        if netmask<= 32:
             message = await ctx.channel.send("Loading")
 
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0'}
@@ -25,29 +25,7 @@ class NetworkCalc(commands.Cog, name="IP / Subnet Calculator"):
             getdoublefonts=gettexts[13].findAll("font")
 
 
-            print(gettexts[0].text)           
-            await message.edit(content="Grabbing completed, preparing message!")
-            embed=discord.Embed()
-            embed.add_field(name=gettexts[0].text, value=gettexts[1].text, inline=True)
-            embed.add_field(name="Binary:", value=gettexts[2].text, inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=gettexts[3].text, value=gettexts[4].text, inline=True)
-            embed.add_field(name="Binary:", value=gettexts[5].text, inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=gettexts[9].text, value=gettexts[10].text, inline=True)
-            embed.add_field(name="Binary:", value=gettexts[11].text+gettexts[12].text[:-1], inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=getdoublefonts[1].text, value=getdoublefonts[2].text, inline=True)
-            embed.add_field(name="Binary:", value=getdoublefonts[3].text, inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=getdoublefonts[4].text, value=getdoublefonts[5].text, inline=True)
-            embed.add_field(name="Binary:", value=getdoublefonts[6].text, inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=getdoublefonts[7].text, value=getdoublefonts[8].text, inline=True)
-            embed.add_field(name="Binary:", value=getdoublefonts[9].text, inline=True)
-            embed.add_field(name="-----------------------------------------", value="-", inline=False)
-            embed.add_field(name=getdoublefonts[10].text, value=getdoublefonts[11].text, inline=True)
-            await ctx.send(embed=embed)
+            await message.edit(content="""```markdown\n""""#"+gettexts[0].text +"\n "+gettexts[1].text+"\n "+gettexts[2].text+"\n> ----------------------------------------- \n#"+gettexts[3].text+" \n "+gettexts[4].text+" \n "+gettexts[5].text+"\n> ----------------------------------------- \n#"+gettexts[9].text+" \n "+gettexts[10].text+" \n "+gettexts[11].text+gettexts[12].text[:-1] +"\n> ----------------------------------------- \n#"+getdoublefonts[1].text+"\n "+getdoublefonts[2].text +"\n "+getdoublefonts[3].text+"\n> ----------------------------------------- \n#"+getdoublefonts[4].text+" \n "+getdoublefonts[5].text+" \n "+getdoublefonts[6].text+"\n> ----------------------------------------- \n#"+getdoublefonts[7].text+" \n "+getdoublefonts[8].text+"\n "+getdoublefonts[9].text+"\n> ----------------------------------------- \n#"+getdoublefonts[10].text+" \n "+getdoublefonts[11].text+" \n```")
         else:
             await ctx.channel.send("*Error, use command as followed: `.ncalc ipadress subnet`*")        
 
